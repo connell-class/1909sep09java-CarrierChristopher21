9902c01f73ad36ffaa9cd6a4717134b46b9eafef
CarrierChristopher21 <CarrierChristopher21@gmail.com>
2019-09-19 15:51:53 -0400

    Update

---
+++ Project 0/src/carDealership/EmployeeData.java
@@ -0,0 +1,15 @@
+package carDealership;
+
+import java.util.Date;
+
+public class EmployeeData {
+	private String FirstName;
+	private String LastName;	
+	private Date DOB;	
+	private int age;		
+	private String email;
+	private String address;
+	private String city;
+	private String zip;
+	private String phone;
+}
